35d9cbc94ecb6da523d49a03d64042786b1a9136
JavierSA <79856488+JavierSA-dev@users.noreply.github.com>
2022-05-12 20:22:38 +0200

    Delete package-info.java

--- docs/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contiene la clase almacén que gestiona un almacén con un arraylist de objetos de la clase Articulo y un test
- * en el que se prueban estas clases
- * 
- * @author Javier Sánchez López
- *
- */
-package almacen;
